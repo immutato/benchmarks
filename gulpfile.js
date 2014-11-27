04d0f6d34e886b544106802edc6b750e7a23f965
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-11-27 20:52:03 +0100

    ported to new dirs

--- gulpfile.js
+++ gulpfile.js
@@ -50,109 +50,17 @@ gulp.task('bench', function() {
         .pipe($.bench());
 });
 
-gulp.task('test', function() {
-    return gulp.src(tests)
-        .pipe($.mocha({
-            grep: '@perf',
-            invert: true
-        }));
-});
-
-gulp.task('cover', function(cb) {
-    gulp.src(sources)
-        .pipe($.istanbul()) // Covering files
-        .on('finish', function() {
-            gulp.src(tests)
-                .pipe($.mocha({
-                    grep: '@perf',
-                    invert: true
-                }))
-                .pipe($.istanbul.writeReports()) // Creating the reports after tests runned
-                .on('end', cb);
-        });
-});
 
 
+gulp.task('dist', function() {
 
-gulp.task('only', function() {
-    return gulp.src('./test/*.js')
-        .pipe($.mocha({
-            grep: '@only'
-        }));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(['./lib/**/*.js', './test/**/*.js'], ['test']);
-});
-
-
-gulp.task('watch-only', function() {
-    gulp.watch(['./lib/**/*.js', './test/**/*.js'], ['only']);
-});
-
-gulp.task('default', ['test', 'watch']);
-
-gulp.task('test-travis', ['test', 'test-phantom-travis'], function() {
-    //require('./run-test-sauce');
-});
-
-var server;
-gulp.task('serve-test', function() {
-    server = $.serve({
-        root: __dirname
-    })();
-});
-
-gulp.task('test-phantom', ['build-test', 'serve-test'], function() {
-
-    var stream = $.mochaPhantomjs();
-    guard(stream);
-    stream.write({
-        path: 'http://localhost:3000/dist/test.html'
-    });
-    stream.end();
-    setTimeout(function() {
-        server.close();
-    }, 1000);
-
-    return stream;
-});
-
-
-gulp.task('test-phantom-travis', function() {
-
-    var stream = $.mochaPhantomjs();
-    guard(stream);
-    stream.write({
-        path: 'http://www.parro.it/immutato/test.html'
-    });
-    stream.end();
-
-
-    return stream;
-});
-
-gulp.task('build-for-browser', function() {
-
-    return gulp.src('./lib/immutato.js')
-        .pipe($.sourcemaps.init())
-        .pipe(guard($.pureCjs({
-            exports: 'immutato',
-            output: 'immutato.js'
-        })))
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('website/dist'));
-});
-
-gulp.task('build-benchmarks', function() {
-
-    return gulp.src('./benchmark/all-benchmarks.js')
+    return gulp.src('./lib/all-benchmarks.js')
         .pipe(guard($.pureCjs({
             external: {
-                '../..': {
+                'immutato-types': {
                     global: 'immutato_prev'
                 },
-                '../lib/immutato': {
+                'immutato': {
                     global: 'immutato'
                 }
             },
@@ -160,33 +68,9 @@ gulp.task('build-benchmarks', function() {
             exports: 'all-benchmarks',
         })))
 
-    .pipe(gulp.dest('website/javascripts'));
-});
-
-
-gulp.task('dist', ['build-for-browser', 'build-benchmarks']);
-
-
-gulp.task('deploy', function() {
-
-    return gulp.src('./lib/immutato.js')
-        .pipe(guard($.pureCjs({
-            exports: 'immutato',
-            output: 'immutato.min.js'
-        })))
-        .pipe($.streamify($.uglify()))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('../website/assets/js'));
 });
 
 
 
 
-gulp.task('build-test', function() {
-
-    return gulp.src('./test/all_test.js')
-        .pipe($.pureCjs({
-            output: 'immutato-test.js'
-        }))
-        // .pipe($.streamify($.uglify()))
-        .pipe(gulp.dest('dist'));
-});
